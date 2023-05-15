a83c1b752d784507fed92197437b97d0f06a3bb9
saulpadilla10 <saulp1021@yahoo.com>
2023-05-15 15:08:06 -0700

    Update

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is you favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Science":
+                    Console.WriteLine("Cool story, Darwin!");
+                    break;
+                case "Math":
+                    Console.WriteLine("Sometimes 2+2=5.");
+                    break;
+                default:
+                    Console.WriteLine("Social bettery at 0% Good-Bye.");
+                    break;
+            }
         }
     }
 }
